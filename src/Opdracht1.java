c9da10716f4dac57cf53ca00521435662d1d1e4e
chester790 <cbornhijm@roc-dev.com>
2020-10-19 15:15:01 +0200

    Driehoek H13 Java

---
+++ src/Opdracht1.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        tekenDriehoek(g,90,50,55,10,25,50);
+    }
+
+    void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
+    }
+
+}
